399422b48d252a40cfb85b117a880430c8832621
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 15:54:39 -0500

    experiments in index.js and updated tasks in readme.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+for (let i=0; i<10; i++) {
+    console.log(i);
+}
+
+for (let i=0; i<10; i++) {
+    if (i === 7)
+        break;
+    console.log(i);
+}
+
+for (let i=10; i>0; i--) {
+    console.log(i);
+}
+
+for (let i=10; i>0; i--) {
+    if (i === 8)
+        continue;
+    if (i ===2)
+        break;
+    console.log(i);
+}
+    */
